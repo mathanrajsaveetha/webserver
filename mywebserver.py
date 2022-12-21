d54b86655f00dcc025099d730401a12f9045fbda
mathanrajsaveetha <www.mr.raj.7010@gmail.com>
2022-12-21 13:47:03 +0530

    updated program

--- mywebserver.py
+++ mywebserver.py
@@ -1,33 +1,3 @@
-# Developing a Simple Webserver
-
-# AIM:
-
-To develop a program to display top 3 programming languages
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation is done
-
-### Step 2:
-
-Design of webserver workflow
-
-### Step 3:
-
-Implementation using Python code
-
-### Step 4:
-
-Serving the HTML pages.
-
-### Step 5:
-
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content ="""
@@ -36,7 +6,7 @@ content ="""
 <h1>Top 3 Programming Languages</h1>
 <h3>Python</h3>
 <h3>C Programming</h3>
-<h3>PL Programming</h3>
+<h3>L Programming</h3>
 </body>
 </html>
 """
@@ -52,8 +22,3 @@ server_address=('',8080)
 httpd=HTTPServer(server_address,WebHandler)
 print("Web server running...")
 httpd.serve_forever()
-```
-## OUTPUT:
-
-## RESULT:
-The program is executed succesfully
